27c31dd45132b99ab4da5990ab9852fd2be8a0bc
amm <aimingming@tbkt.cn>
2018-07-03 14:07:13 +0800

    add 显示版本号

--- app/src/main/java/com/amm/helloworld/MainActivity.java
+++ app/src/main/java/com/amm/helloworld/MainActivity.java
@@ -1,13 +1,23 @@
 package com.amm.helloworld;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+    private TextView vn_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vn_tv = (TextView)findViewById(R.id.vn_tv);
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),0);
+            vn_tv.setText(packageInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
